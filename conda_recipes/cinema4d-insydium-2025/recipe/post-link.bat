cf1eb6a28e2a808b188435b8e69790bc0283db30
Dan Marum <dan@gunpowder.tech~>
2025-09-21 15:45:17 -0700

    x

--- conda_recipes/cinema4d-insydium-2025/recipe/post-link.bat
+++ conda_recipes/cinema4d-insydium-2025/recipe/post-link.bat
@@ -1,7 +1,6 @@
 rem Copy staged plugin to the user plugins_x dir
-for /d %%D in ("%APPDATA%\Maxon\*2025*_x") do (
-    xcopy "%PREFIX%\insydium_staging" "%%D\" /E /I /H /Y
-)
-dir "%APPDATA%\Maxon\*2025*_x\*"
+rem for /d %%D in ("%APPDATA%\Maxon\cinema4d_*_x") do (
+rem     xcopy /E /I /H /Y "%PREFIX%\insydium_staging" "%%D\plugins\"
+rem )
 rem Pre-emptively initialize connction to license server
 curl "https://license.insydium.net"
